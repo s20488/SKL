b60b106e3f3af3a68c996cea55277a95d7009a66
Anastasiia Ponkratova <56721899+s20488@users.noreply.github.com>
2025-03-04 04:02:35 +0100

    Update zadanie_1_6.scala

--- lab6/zadanie_1_6.scala
+++ lab6/zadanie_1_6.scala
@@ -1,4 +1,4 @@
-import org.apache.pekko.actor._
+import akka.actor._
 
 case class Start(next: ActorRef)
 case object Ping
@@ -9,11 +9,11 @@ class Player extends Actor {
     case Start(next) =>
       println(s"${self.path.name} zaczyna grę z ${next.path.name}")
       next ! Ping
-
+    
     case Ping =>
       println(s"${self.path.name}: Otrzymałem 'Ping', odpowiadam 'Pong'")
       sender() ! Pong
-
+    
     case Pong =>
       println(s"${self.path.name}: Otrzymałem 'Pong', odpowiadam 'Ping'")
       sender() ! Ping
@@ -22,24 +22,12 @@ class Player extends Actor {
 
 @main
 def zad1(): Unit = {
-  val system = ActorSystem("PingPongSystem")
-
-  println("Rozpoczynamy grę w ping-ponga dla dwóch graczy.")
-  val player1 = system.actorOf(Props[Player](), "player1")
-  val player2 = system.actorOf(Props[Player](), "player2")
-  player1 ! Start(player2)
-
-  //  println("\nRozpoczynamy grę w ping-ponga dla trzech graczy.")
-  //  val playerA = system.actorOf(Props[Player](), "playerA")
-  //  val playerB = system.actorOf(Props[Player](), "playerB")
-  //  val playerC = system.actorOf(Props[Player](), "playerC")
-  //  playerA ! Start(playerB)
-  //  playerB ! Start(playerC)
-  //  playerC ! Start(playerA)
-  //
-  //  println("\nRozpoczynamy grę w ping-ponga dla listy graczy.")
-  //  val players = (1 to 5).map(i => system.actorOf(Props[Player](), s"player_$i")).toList
-  //  for (i <- players.indices) {
-  //    players(i) ! Start(players((i + 1) % players.size))
-  //  }
+  ActorSystem("PingPongSystem").actorOf(Props(new Actor {
+    override def preStart(): Unit = {
+      context.actorOf(PropsPlayer, "player1") ! Start(
+        context.actorOf(PropsPlayer, "player2")
+      )
+    }
+    def receive: Receive = Actor.emptyBehavior
+  }))
 }
